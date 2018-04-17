fc1fe7e7cd93731637457b771bfeac2533c4d163
candy <perfectpencil1@gmail.com>
2018-04-16 23:08:36 -0600

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Triangles_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Triangles_cpp.dir/main.cpp.obj"
+  "Triangles_cpp.pdb"
+  "Triangles_cpp.exe"
+  "Triangles_cpp.exe.manifest"
+  "libTriangles_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Triangles_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
